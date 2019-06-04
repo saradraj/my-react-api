22b35bd00b22755102105f09c32a43707574a52f
srajbhandari <srajbhandari@contractor.ea.com>
2019-06-03 20:14:00 -0500

    sample react api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pictures: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api/?results=10")
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        let pictures = data.results.map((pic) => {
+          return (
+            <div key={pic.results}>
+              <img src={pic.picture.medium} alt="heroes" />
+            </div>
+          );
+        })
+        this.setState({ pictures: pictures });
+       // console.log("state", this.state.pictures);
+      });
+  }
+
+  render() {
+    return(
+      <div className="App">
+      {this.state.pictures}
+      </div>
+    )
+  }
 }
 
 export default App;
